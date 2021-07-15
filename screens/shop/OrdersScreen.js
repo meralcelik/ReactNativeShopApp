b6082c8b3faa79de6f16623c32da7af4f05438cb
meralcelik <meralcelik1444@gmail.com>
2021-07-15 14:39:16 +0300

    orderıtem bittti

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -5,6 +5,8 @@ import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 
+import OrderItem from '../../components/shop/OrderItem';
+
 const OrdersScreen = props => {
   const orders = useSelector(state => state.orders.orders);
 
@@ -12,7 +14,13 @@ const OrdersScreen = props => {
     <FlatList
       data={orders}
       keyExtractor={item => item.id}
-      renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={itemData => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+          items={itemData.item.items}
+        />
+      )}
     />
   );
 };
@@ -21,20 +29,17 @@ OrdersScreen.navigationOptions = navData => {
   return {
     headerTitle: 'Your Orders',
     headerLeft: (
-        <HeaderButtons HeaderButtonComponent={HeaderButton}>
-          <Item
-            title="Menu"
-            iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
-            onPress={() => {
-              navData.navigation.toggleDrawer();
-            }}
-          />
-        </HeaderButtons>
-      ),
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    )
   };
 };
-OrdersScreen.navigationOptions ={
-  headerTitle :'your caorderrt'
-  };
 
 export default OrdersScreen;
